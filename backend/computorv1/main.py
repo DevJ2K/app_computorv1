02c05fa6b472cac5801d81c4927acf221328c74c
DevJ2K <ajavontheopro@gmail.com>
2024-08-23 15:23:39 +0200

    🧹 : Cleaning repository...

--- backend/computorv1/main.py
+++ backend/computorv1/main.py
@@ -1,7 +1,13 @@
 #!/usr/bin/env python3
 
+import sys
+from Computor import Computor
+
 def basic_function():
 	print("It's work !")
 
 if __name__ == "__main__":
+	if (len(sys.argv) != 2):
+		exit(0)
+	Computor()
 	basic_function()
