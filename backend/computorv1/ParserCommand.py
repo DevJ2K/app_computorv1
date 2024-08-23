740f41688cc153db4221222bdbb2e6dc3ead940d
DevJ2K <ajavontheopro@gmail.com>
2024-08-23 20:49:15 +0200

    ✅ : Add some unittest

--- backend/computorv1/ParserCommand.py
+++ backend/computorv1/ParserCommand.py
@@ -3,16 +3,22 @@
 # - len 3 min
 # - 1 equal (=) avec 2 blocs et min 1 de len
 
+from ErrorManager import ErrorManager
 
 def is_an_equation(polynomial: str) -> bool:
-	result = polynomial.split()
+	result = polynomial.split("=")
+	# print(result)
 	return (
 		len(result) == 2
-		and len(result[0]) > 1
-		and len(result[1]) > 1
+		and len(result[0]) >= 1
+		and len(result[1]) >= 1
 		)
 
 
 def is_polynomial_form(polynomial: str) -> bool:
-
-	pass
+	if (len(polynomial) < 3):
+		ErrorManager("Invalid polynomial format", 0, False)
+		return (False)
+	if (is_an_equation(polynomial) == False):
+		ErrorManager("Invalid polynomial format", 0, False)
+		return (False)
--- backend/computorv1/tests/test_ParserCommand.py
+++ backend/computorv1/tests/test_ParserCommand.py
@@ -5,4 +5,16 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 from ParserCommand import *
 
+class TestParsingChecker(unittest.TestCase):
+	def test_is_an_equation(self):
+		self.assertEqual(is_an_equation("3 = 4"), True)
+		self.assertEqual(is_an_equation("3 4"), False)
+		self.assertEqual(is_an_equation("3==4"), False)
+		self.assertEqual(is_an_equation("0 = 3 = 4"), False)
+		self.assertEqual(is_an_equation("3=4"), True)
 
+# RUN TEST
+# if __name__ == "__main__":
+# 	unittest.main()
+
+# python -m unittest -v test_ParserCommand
