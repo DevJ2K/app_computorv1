af366c66b0a11637bb34ba386c74abf811118a51
DevJ2K <ajavontheopro@gmail.com>
2024-08-25 00:03:52 +0200

    💭 : Create other methods in Monomial.py

--- backend/computorv1/Monomial.py
+++ backend/computorv1/Monomial.py
@@ -6,3 +6,27 @@ class Monomial:
 		self.coefficient: float = 1
 		self.sign: str = '+'
 
+
+	def __str__(self) -> str:
+		return f"{self.sign}{self.coefficient} * X^{self.degree}"
+
+	def __repr__(self) -> str:
+		return self.__str__()
+
+	def __eq__(self, value: object) -> bool:
+		if isinstance(value, Monomial):
+			return (
+				self.sign == value.sign and
+                self.coefficient == value.coefficient and
+                self.degree == value.degree
+			)
+		return False
+
+
+if __name__ == "__main__":
+	print(Monomial("+5 * X^0 ")) # '+5 *X^0'
+	print(Monomial(" 5 * X^2 ")) # '+5 *X^0'
+	print(Monomial("+5 * X^20 ")) # '+5 *X^0'
+	print(Monomial("- 5 * X^2 ")) # '+5 *X^0'
+	print(Monomial("- 9.3 * X^2 ")) # '+5 *X^0'
+
