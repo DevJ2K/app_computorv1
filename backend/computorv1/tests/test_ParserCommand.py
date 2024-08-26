34fa78b1a445877cec83fe791b6f61dafdf61b18
DevJ2K <ajavontheopro@gmail.com>
2024-08-26 12:53:14 +0200

    ✅ : Add more tests for 'is_an_equation' function.

--- backend/computorv1/tests/test_ParserCommand.py
+++ backend/computorv1/tests/test_ParserCommand.py
@@ -6,23 +6,46 @@ from ParserCommand import *
 
 
 def test_valid_equation():
-	assert is_polynomial_form("3 = 4") == True
-	assert is_polynomial_form("3=4") == True
+	assert is_an_equation("3 = 4") == True
+	assert is_an_equation("3=4") == True
+	assert is_an_equation("3 * X^0=4") == True
+	assert is_an_equation("X^0=X^0") == True
+	assert is_an_equation("-  3*X^0=   4") == True
+	assert is_an_equation("3 * X^0=4*X^3 + 2 * X^3") == True
+	assert is_an_equation("3 * X^0=4") == True
+	assert is_an_equation("3 * X^4 = X^0") == True
+	assert is_an_equation("-3 * X^4 = X^0") == True
+	assert is_an_equation("-3 = X^34 + 3*X^3") == True
+	assert is_an_equation("-3 = X^34 + 3*X") == True
+	assert is_an_equation("0.0 + 45*x^2 + 451*x = 0") == True
+	assert is_an_equation("	0.0    + 45	*	x^2 + 451	*   x = 	0") == True
 
 def test_invalid_equation():
-	assert is_polynomial_form("3 4") == False
-	assert is_polynomial_form("3==4") == False
-	assert is_polynomial_form("0 = 3 = 4") == False
-	assert is_polynomial_form("3 = ") == False
+	assert is_an_equation("3 4") == False
+	assert is_an_equation("3==4") == False
+	assert is_an_equation("0 = 3 = 4") == False
+	assert is_an_equation("3 * X^0=4*X^3 2 * X^3 =") == False
+	assert is_an_equation("3 = ") == False
+	assert is_an_equation("3 = X^") == False
+	assert is_an_equation("3 * X^4 == X^0") == False
+	assert is_an_equation("--3 * X^4 == X^0") == False
+	assert is_an_equation("- -3 * X^4 == X^0") == False
+	assert is_an_equation("- -3 * X^4 == X ^0") == False
+	assert is_an_equation("- -3 * X^4 == X ^ 0") == False
+	assert is_an_equation("- -3 * X^4 == X^^0") == False
+	assert is_an_equation("- -3 * X^4 == X^^-10") == False
+	assert is_an_equation("	0..0    + 45	*	x^2 + 451	*   x = 	0") == False
+	assert is_an_equation("	0. 0    + 45	*	x^2 + 451	*   x = 	0") == False
+	assert is_an_equation("	0.0    + 45	*	x^2 + 451	*   *x = 	0") == False
 
-def test_valid_polynomial_form():
+def _test_valid_polynomial_form():
 	assert is_polynomial_form("5 * X^0 + 4 * X^1 - 9.3 * X^2 = 0") == True
 	assert is_polynomial_form("5 + 4 * X^1 - 9.3 * X^2 = 0") == True
 	assert is_polynomial_form("5 * X^0 + 4 * X^1 - 9.3 * X^2 = x^2") == True
 	assert is_polynomial_form("5*X^0+4*X^1-9.3*X^2=0") == True
 	assert is_polynomial_form("	 -5     *	 X^0   +  4 	*  X^1    -    9.3   *    X^2  = 0") == True
 
-def test_invalid_polynomial_form():
+def _test_invalid_polynomial_form():
 	assert is_polynomial_form("5 * X^0 + 4 * X^1 - 9.3 * X^2 = ") == False
 	assert is_polynomial_form("--5 * X^0 + 4 * X^1 - 9.3 * X^2 = 0") == False
 	assert is_polynomial_form("5 * X^    0 + 4 * X^1 - 9.3 * X^2 = 0") == False
@@ -30,8 +53,11 @@ def test_invalid_polynomial_form():
 
 
 
-def test_convert_to_list():
-	assert convertToMonomialStrList("5 * X^0 + 4 * X^1 - 9.3 * X^2") == ['+5 * X^0 ', '+ 4 * X^1 ', '- 9.3 * X^2']
+# def test_convert_to_list():
+# 	assert convertToMonomialStrList("5 * X^0 + 4 * X^1 - 9.3 * X^2") == ['5*X^0', '+4*X^1 ', '-9.3*X^2']
+# 	assert convertToMonomialStrList("	 5*X^0    +  	4*X^1-	 9.3*	 X^2") == ['5*X^0', '+4*X^1 ', '-9.3*X^2']
+	# assert convertToMonomialStrList("5 * X^0 + 4 * X^1 - 9.3 * X^2") == ['5*X^0', '+4*X^1 ', '-9.3*X^2']
+	# assert convertToMonomialStrList("5 * X^0 + 4 * X^1 - 9.3 * X^2") == ['5*X^0', '+4*X^1 ', '-9.3*X^2']
 
 # "0.0 + 45*x^2 + 451*x = 0"
 
