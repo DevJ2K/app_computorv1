d90fdf01c7488e08a400b7be6221f0b5cfb0b7a5
DevJ2K <ajavontheopro@gmail.com>
2024-08-26 13:11:45 +0200

    ✅ : Add more tests for 'is_polynomial_form' function and all works !

--- backend/computorv1/tests/test_ParserCommand.py
+++ backend/computorv1/tests/test_ParserCommand.py
@@ -38,18 +38,28 @@ def test_invalid_equation():
 	assert is_an_equation("	0. 0    + 45	*	x^2 + 451	*   x = 	0") == False
 	assert is_an_equation("	0.0    + 45	*	x^2 + 451	*   *x = 	0") == False
 
-def _test_valid_polynomial_form():
+def test_valid_polynomial_form():
 	assert is_polynomial_form("5 * X^0 + 4 * X^1 - 9.3 * X^2 = 0") == True
 	assert is_polynomial_form("5 + 4 * X^1 - 9.3 * X^2 = 0") == True
 	assert is_polynomial_form("5 * X^0 + 4 * X^1 - 9.3 * X^2 = x^2") == True
 	assert is_polynomial_form("5*X^0+4*X^1-9.3*X^2=0") == True
 	assert is_polynomial_form("	 -5     *	 X^0   +  4 	*  X^1    -    9.3   *    X^2  = 0") == True
+	assert is_an_equation("0.0 + 10*x^2 + 146*x = 0") == True
+	assert is_an_equation("	0.0    + 10	*	x^2 + 146	*   x = 	0") == True
 
-def _test_invalid_polynomial_form():
+def test_invalid_polynomial_form():
 	assert is_polynomial_form("5 * X^0 + 4 * X^1 - 9.3 * X^2 = ") == False
 	assert is_polynomial_form("--5 * X^0 + 4 * X^1 - 9.3 * X^2 = 0") == False
 	assert is_polynomial_form("5 * X^    0 + 4 * X^1 - 9.3 * X^2 = 0") == False
 	assert is_polynomial_form("5 * + 4 * X^1 - 9.3 * X^2 = 0") == False
+	assert is_polynomial_form("5 * X^-12 + 4 * X^1 - 9.3 * X^2 = 0") == False
+	assert is_polynomial_form("5 ** X^12 + 4 * X^1 - 9.3 * X^2 = 0") == False
+	assert is_polynomial_form("5 * X^12 + 4 * X^1 - 9. * X^2 = 0") == False
+	assert is_polynomial_form("5 * X^12 + 4 * X^1 - 9.-43 * X^2 = 0") == False
+	assert is_polynomial_form("5 * X^12 + 4 * X^1 - 9.43 * X^2 = --0^4") == False
+	assert is_polynomial_form("5 * X^12 + 4 * X^1 - 9.43 * X^2  0^4") == False
+	assert is_polynomial_form("5 * X^12 + 4 * X^1 - 9.43 * X^2 == 0^4") == False
+	assert is_polynomial_form("5- = X^1") == False
 
 
 
