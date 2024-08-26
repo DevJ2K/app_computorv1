6f5f85ac36fed13abd1944bbb49aae016063ff04
DevJ2K <ajavontheopro@gmail.com>
2024-08-26 18:44:10 +0200

    ✅ : Add more tests for 'convertToMonomialList' function.

--- backend/computorv1/tests/test_ParserCommand.py
+++ backend/computorv1/tests/test_ParserCommand.py
@@ -1,5 +1,6 @@
 import sys
 import os
+import pytest
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 from ParserCommand import *
@@ -61,6 +62,42 @@ def test_invalid_polynomial_form():
 	assert is_polynomial_form("5 * X^12 + 4 * X^1 - 9.43 * X^2 == 0^4") == False
 	assert is_polynomial_form("5- = X^1") == False
 
+
+def _strMonomialList(polynomial: str) -> str:
+	string = ""
+	monomial_list: list[Monomial] = convertToMonomialList(polynomial)
+	for i in range(len(monomial_list)):
+		coef = monomial_list[i].coefficient
+		string = string + str(int(coef) if int(coef) == coef else coef)
+		string += " * X^"
+		string += str(monomial_list[i].degree)
+		# print(monomial_list[i].degree)
+		if (i + 1 < len(monomial_list)):
+			string += " "
+	return string
+
+def test_valid_convert_to_monomial_list_1_item():
+	assert _strMonomialList("5 * X^0") == '5 * X^0'
+	assert _strMonomialList("	5 * 	X^0  ") == '5 * X^0'
+	assert _strMonomialList("5") == '5 * X^0'
+	assert _strMonomialList("	5.0*X^0") == '5 * X^0'
+	assert _strMonomialList("5.00 * 	X^015	") == '5 * X^15'
+	assert _strMonomialList("5 *       	x^130	") == '5 * X^130'
+	assert _strMonomialList("5 * x^0	") == '5 * X^0'
+	assert _strMonomialList("5 * X^12") == '5 * X^12'
+	assert _strMonomialList("5 	*	 X^5") == '5 * X^5'
+	assert _strMonomialList("55425426426426426426246246 * X^0") == '55425426426426426140917760 * X^0'
+	assert _strMonomialList("5542542642642-6426426246246 * X^0") == "5542542642642 * X^0 -6426426246246 * X^0"
+
+def test_invalid_convert_to_monomial_list_1_item():
+	invalid_polynomial = [
+		"5 * Xa^0",
+		"5- * X^0",
+		"5542542642642*6426426246246 * X^0"
+	]
+	for monomial in invalid_polynomial:
+		with pytest.raises(InvalidPolynomialError):
+			_strMonomialList(monomial)
 
 
 # def test_convert_to_list():
