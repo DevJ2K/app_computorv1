b25d4ddf4237737b58a03d8f9897d2959feab07a
DevJ2K <ajavontheopro@gmail.com>
2024-08-26 13:12:44 +0200

    ✅ : Polynomial function checker works !

--- backend/computorv1/ParserCommand.py
+++ backend/computorv1/ParserCommand.py
@@ -5,12 +5,17 @@
 
 from ErrorManager import ErrorManager
 from Monomial import Monomial
+import re
 
-# Use RegEx !
-# Use RegEx !
-# Use RegEx !
-# Use RegEx !
 def is_an_equation(polynomial: str) -> bool:
+	side_regex = r"(\s*(?:(?:[-+]?\s*\d+(?:\.\d+)?\s*(?:\*\s*[xX](?:\^\d+)?)?)|(?:[xX](?:\^\d+)?)))+"
+	regex = side_regex + r"\s*=\s*" + side_regex + "$"
+
+	match = re.match(regex, polynomial)
+	if match:
+		return True
+	return False
+
 	result = polynomial.split("=")
 	# print(result)
 	return (
@@ -21,14 +26,32 @@ def is_an_equation(polynomial: str) -> bool:
 
 
 def is_polynomial_form(polynomial: str) -> bool:
-	# Use RegEx !
 	if (len(polynomial) < 3):
-		ErrorManager("Invalid polynomial format", 0, False)
-		return (False)
+		return False
 	if (is_an_equation(polynomial) == False):
-		ErrorManager("Invalid polynomial format", 0, False)
-		return (False)
-	return (True)
+		return False
+
+	# regex = r"(\s*[-+]?\s*\d+(?:\.\d+)?\s*(?:\*\s*X\^\d+)?)"
+	regex = r"\s*(?:(?:[-+]?\s*\d+(?:\.\d+)?\s*(?:\*\s*[xX](?:\^\d+)?)?)|(?:[xX](?:\^\d+)?))"
+	match = re.findall(regex, polynomial)
+	if match:
+		rm_equal_polynomial = polynomial.replace('=', '')
+		rm_whitespace_polynomial = re.sub(r"\s", "", rm_equal_polynomial)
+
+		# print(rm_whitespace_polynomial)
+		# print(re.sub(r"\s", "",''.join(match)))
+		# print(match)
+
+
+		# formatted_match = [re.sub(r"\s", "", item) for item in match]
+		# print(formatted_match)
+
+		if len(rm_whitespace_polynomial) == len(re.sub(r"\s", "",''.join(match))):
+			return True
+		else:
+			return False
+
+	return False
 
 # 5 * X^0 + 4 * X^1 - 9.3 * X^2 => {
 # 5 * X^0
@@ -39,51 +62,32 @@ def is_polynomial_form(polynomial: str) -> bool:
 
 
 
+# def convertToMonomialList(monomial_str_list: list[str]) -> list[Monomial]:
+# 	monomial_list = []
+# 	for monomial_str in monomial_str_list:
+# 		monomial_list.append(Monomial(monomial_str))
+# 	return monomial_list
+
 def convertToMonomialList(monomial_str_list: list[str]) -> list[Monomial]:
-	monomial_list = []
-	for monomial_str in monomial_str_list:
-		monomial_list.append(Monomial(monomial_str))
-	return monomial_list
+	pass
 
 def convertToMonomialStrList(side: str) -> list[str]:
-	# Use RegEx !
-	previousSign = side[0] if side[0] == "-" else "+"
-	side = side.removeprefix("+")
-	side = side.removeprefix("-")
-	currentMonomial = ""
-	str_monomial_list: list[str] = []
-
-	i = 0
-	while (i < len(side)):
-		if (side[i] == "+" and i != 0):
-			str_monomial_list.append(previousSign + currentMonomial)
-			previousSign = "+"
-			currentMonomial = ""
-			i += 1
-		elif (side[i] == "-" and i != 0):
-			str_monomial_list.append(previousSign + currentMonomial)
-			previousSign = "-"
-			currentMonomial = ""
-			i += 1
-		else:
-			currentMonomial += side[i]
-			i += 1
-	if (currentMonomial != ""):
-		str_monomial_list.append(previousSign + currentMonomial)
-
-	print(str_monomial_list)
-	# print(str_monomial_list)
-	# print(side)
-	return (str_monomial_list)
-
+	pass
 
 if (__name__ == "__main__"):
-	l1str = convertToMonomialStrList("5 * X^0 + 4 * X^1 - 9.3 * X^2")
-	l1 = convertToMonomialList(l1str)
-	print(l1)
+	# is_an_equation("0 = 3 = 4")
+	# is_polynomial_form("5 * X^0")
+	# is_polynomial_form("5 * X^0 + 4 * X^1 - 9.3 * X^2 = 0")
+	# is_polynomial_form("X^0")
+	# is_polynomial_form("5 * X^0 + 4 * X^1 - 9.3 * X^2 - 5 * X^0")
+	# is_polynomial_form("5 * X^0 + 4 * X^ - 9.3 * X^2 - 5 * X^0")
+	# l1str = convertToMonomialStrList("5 * X^0 + 4 * X^1 - 9.3 * X^2")
+	# l1 = convertToMonomialList(l1str)
+	# print(l1)
 	# print("==================")
 	# convertToMonomialStrList("+ 4 * X^1 - 9.3 * X^2")
-	print("==================")
+	# print("==================")
 	# convertToMonomialStrList("+5 * X^0 + 4 * X^1 - 9.3 * X^2 - 3 * X^3")
 	# print("==================")
 	# convertToMonomialStrList("-5 * X^0 + 4 * X^1 - 9.3 * X^2")
+	pass
