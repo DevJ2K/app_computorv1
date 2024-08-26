8416c96359bcc6e5eec4eb78aa97c11e7c2a0825
DevJ2K <ajavontheopro@gmail.com>
2024-08-26 17:35:18 +0200

    ✅ : Init left hand side & right hand side from str.

--- backend/computorv1/Computor.py
+++ backend/computorv1/Computor.py
@@ -1,22 +1,39 @@
 from Colors import *
-from ParserCommand import is_polynomial_form
+from ParserCommand import is_polynomial_form, convertToMonomialList
+from Monomial import Monomial
+from ErrorManager import *
 
 class Computor:
 	"""Computor class : To manipulate polynomial second or lower degree equation."""
-
 
 	def __init__(self, polynomial: str) -> None:
 		self.polynomial = polynomial
 		self.reduced_from: str | None = None
 
-		self.lhs: str | None = None
-		self.rhs: str | None = None
-		self.__priv = 12
-		self._priv = 14
+		self.lhs: list[Monomial] | None = None
+		self.rhs: list[Monomial] | None = None
+
+		self.__initEquation()
 
 
 	def __str__(self) -> str:
 		return (f"Polynomial => {BBLACK}{self.polynomial}")
+
+	def __initEquation(self) -> None:
+		if (is_polynomial_form(self.polynomial) == False):
+			raise InvalidPolynomialError
+		split_polynomial = self.polynomial.split("=")
+
+		self.lhs = convertToMonomialList(split_polynomial[0]);
+		self.rhs = convertToMonomialList(split_polynomial[1]);
+
+		if self.lhs is None or self.rhs is None:
+			raise InvalidPolynomialError
+
+		print(self.lhs)
+		print(self.rhs)
+
+
 
 	def __reducePolynomial(self) -> bool:
 		polynomial = self.polynomial
@@ -31,3 +48,6 @@ class Computor:
 
 	def solve(self) -> str:
 		return ""
+
+if __name__ == "__main__":
+	Computor("	 -5     *	 X^0   +  4 	*  X^1    -    9.3   *    X^2  = 0")
