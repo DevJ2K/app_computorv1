f55aad67b95d18986552cfc677e4da3752935e6a
DevJ2K <ajavontheopro@gmail.com>
2024-08-29 19:24:11 +0200

    ✅ : Fix Monomial parsing.

--- backend/computorv1/Monomial.py
+++ backend/computorv1/Monomial.py
@@ -9,6 +9,7 @@ class Monomial:
 
 	def __init__(self, monomial: str) -> None:
 		self.monomial: str = monomial
+		# self.monomial: str = re.sub(r"\s*", "", monomial)
 		self.degree: int = 0
 		self.coefficient: float = 0
 
@@ -19,6 +20,7 @@ class Monomial:
 			raise MonomialError("Not a monomial format.")
 
 	def __str__(self) -> str:
+		# return self.monomial
 		return f"{self.coefficient} * X^{self.degree}"
 
 	def __repr__(self) -> str:
@@ -34,7 +36,7 @@ class Monomial:
 
 	def __is_monomial_format(self) -> bool:
 		monomial = self.monomial
-		regex = fr"{MyRegex['monomial']}\s*$"
+		regex = MyRegex['monomial'] + r"\s*$"
 		match = re.match(regex, monomial)
 		if match:
 			return True
@@ -48,14 +50,22 @@ class Monomial:
 
 		regex = MyRegex['monomial_coefficient']
 		match = re.match(regex, monomial)
+		print(match)
 		if match:
 			try:
 				trim_match = re.sub(r'\s', '', match.group())
+				# print(trim_match)
 				coefficient = float(trim_match)
 				# print(f"{match.groups()} => {coefficient}")
 				self.coefficient = coefficient
 			except:
-				raise MonomialConvertError("Unable to convert the value to float.")
+				print(trim_match)
+				if trim_match == "-":
+					self.coefficient = -1.0
+				elif trim_match == "+":
+					self.coefficient = 1.0
+				else:
+					raise MonomialConvertError("Unable to convert the value to float.")
 		else:
 			regex = MyRegex['monomial_degree']
 			match = re.search(regex, monomial)
@@ -81,19 +91,23 @@ class Monomial:
 
 
 if __name__ == "__main__":
-	# print(int("-    5"))
+	print(Monomial("+5 * X^14 "))
+	print(Monomial(" "))
 	print(Monomial("+  5 * X^1 "))
+	# print(Monomial("++ 5 * X^1 "))
+	print(Monomial("+X^1 "))
+	print(Monomial("-X^1 "))
 	# print(Monomial("+ * X^0 "))
 	# print(Monomial("++5 * X^0 "))
-	print(Monomial(" +5 * X^3 "))
-	print(Monomial(" +5.4 * X^4 "))
-	print(Monomial("+545 * X^12 "))
-	print(Monomial("+545* X^43 "))
-	print(Monomial("-5*X^0"))
+	# print(Monomial(" +5 * X^3 "))
+	# print(Monomial(" +5.4 * X^4 "))
+	# print(Monomial("+545 * X^12 "))
+	# print(Monomial("+545* X^43 "))
+	# print(Monomial("-5*X^0"))
 	# print(Monomial("--5*X^0"))
-	print(Monomial(" 5*X^0"))
-	print(Monomial(" 5"))
-	print(Monomial(" 5 * X"))
+	# print(Monomial(" 5*X^0"))
+	# print(Monomial(" 5"))
+	# print(Monomial(" 5 * X"))
 	# print(Monomial(" a5*X^0"))
 	# print(Monomial(" 5 * X^2 "))
 	# print(Monomial("+5 * X^20 "))
