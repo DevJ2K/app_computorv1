4a09dab2f57121a4901023fb3f84b77cfca84f75
DevJ2K <ajavontheopro@gmail.com>
2024-08-30 16:35:12 +0200

    💭 : Edit main.py

--- backend/computorv1/main.py
+++ backend/computorv1/main.py
@@ -2,15 +2,26 @@
 
 import sys
 from Computor import Computor
-from ErrorManager import ErrorManager
+from ErrorManager import *
+from Colors import *
 
 def basic_function():
 	print("It's work !")
 
+def print_error(message: str) -> None:
+	print(f"{BHRED}Error: {RESET}{BRED}{message}{RESET}")
+
 if __name__ == "__main__":
 	if len(sys.argv) != 2:
-		ErrorManager("argc", 1)
-	polynomial = Computor("5 * X^0 + 4 * X^1 - 9.3 * X^2 = x^2")
-	# print(polynomial._priv)
-	# print(polynomial._Computor__priv)
-	# print(polynomial)
+		print(f"{BHGREEN}Usage:{RESET} {BHWHITE}./main.py {RESET}{BHCYAN}<polynomial>{RESET}")
+	else:
+		try:
+			polynomial = Computor(sys.argv[1])
+			polynomial.solve()
+			print(polynomial.display_solution())
+		except InvalidPolynomialError:
+			print_error("The provided polynomial is invalid. Please check the format and try again.")
+		except MonomialError:
+			print_error("There is an issue with one or more monomials in the polynomial. Please review and correct them.")
+		except MonomialConvertError:
+			print_error("Could not convert one or more monomials. Ensure that the polynomial is properly formatted.")
